c9f935e1e911de0061b812993ac216be9cb9f93f
Hubert Łępicki <hubert.lepicki@amberbit.com>
2018-12-15 12:33:31 +0100

    Add react

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import "./index.scss"
+import ReactDOM from "react-dom";
+import React from "react";
 
-(() => alert("Hello, ES6 world!"))()
+const reactRoot = document.createElement("div");
+reactRoot.setAttribute("id", "react-root");
+document.body.appendChild(reactRoot);
 
+const App = (props) => <div>Hello, React!</div>
+
+ReactDOM.render(<App />, reactRoot);
